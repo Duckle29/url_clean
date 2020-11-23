6f5c3a6bce9c28af8195db228190ac6ccf724b16
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-11-23 15:19:19 +0100

    Fixed amazon regex

--- url_clean.user.js
+++ url_clean.user.js
@@ -9,11 +9,12 @@
 // @homepageURL https://github.com/Duckle29/url_clean
 // @downloadURL https://github.com/Duckle29/url_clean/raw/main/url_clean.user.js
 // @updateURL   https://github.com/Duckle29/url_clean/raw/main/url_clean.user.js
-// @version     1.1
+// @version     1.1.1
 //
 // @include     /^https?:\/\/(?:www\.)?([a-zA-Z]{2,3}\.)?aliexpress\.com\/(item|store\/product)\/.*/
 // @include     /^https?:\/\/(?:www\.)?ebay\.(?:co.)?[a-zA-Z]{2,3}\/itm/
-// @include     /^https?:\/\/(?:www\.)?amazon\.(?:co.)?[a-zA-Z]{2,3}\/
+// @include     /^https?:\/\/(?:www\.)?amazon\.(?:co.)?[a-zA-Z]{2,3}\//
+// @history     1.1.1 Fixed regex for Amazon
 // @history     1.1 Added Amazon
 // @history     1.0 Initial release
 // ==/UserScript==
@@ -25,7 +26,7 @@
   [
     /^(https?:\/\/(?:www\.)?ebay\.(?:(?:co.)?[a-zA-Z]{2,3})\/itm)(?:\/[0-9a-zA-Z\-]+)(\/\d+)/,
     /^(https?:\/\/(?:[a-zA-Z]{2,3}\.)?aliexpress.com\/(?:item|store\/product))(\/[0-9_]+[.]html(?=$|[?]))/,
-    /^(https?:\/\/(?:www\.)?amazon\.(?:co.)?[a-zA-Z]{2,3}\/.*\/dp\/)(.*)\//
+    /^(https?:\/\/(?:www\.)?amazon\.(?:co.)?[a-zA-Z]{2,3}\/.*\/dp\/)(.*)(?:\\)?\?/
   ];
 
   sites.forEach(regReplace)
